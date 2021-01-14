85cad50ff06f86c873a24c5dd215421afb2f6ef9
TuCB <tu.trang@codebox.vn>
2021-01-15 00:46:08 +0700

    Middle truncate address string

--- AlphaWallet/Tokens/ViewControllers/VelasTokensViewController.swift
+++ AlphaWallet/Tokens/ViewControllers/VelasTokensViewController.swift
@@ -119,7 +119,7 @@ extension VelasTokensViewController {
         if server != nil {
             address = VelasConvertUtil.convertVlxStringIfNeed(server: server!, address: address)
         }
-        let serverInfo = HeaderInfo(server: server, image: nil, name: server?.name ?? "Other", subTitle: address)
+        let serverInfo = HeaderInfo(server: server, image: nil, name: server?.name ?? "Other", subTitle: address.truncatedMiddle)
         let serverHeaderConfig: HeaderServer = .Server(serverInfo)
         let configuration: HeaderServer = isSectionMode ? serverHeaderConfig : .Hide(color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
         header.configHeader(configuration)
